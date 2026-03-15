20b62a7dab401c1a15a59a2788942db8c632dba6
Jamil Chabane <jamilchabane@ba:59:1f:e7:1b:d2.home>
2026-03-15 15:56:28 +0100

    fix buffer v2

--- publishers/buffer.js
+++ publishers/buffer.js
@@ -1,4 +1,22 @@
-cd ~/Downloads/autopost
-curl -o publishers/buffer.js https://raw.githubusercontent.com/jamil844/autopost-backend/main/publishers/buffer.js
-cd ~/Downloads/autopost-v2
-cd ~/Downloads/autopost
+const axios = require('axios');
+const BUFFER_API = 'https://api.bufferapp.com/1';
+
+async function getBufferChannels(accessToken) {
+  const response = await axios.get(`${BUFFER_API}/profiles.json`, {
+    params: { access_token: accessToken }
+  });
+  return response.data.map(p => ({ id: p.id, name: p.formatted_username, platform: p.service }));
+}
+
+async function publishNowWithBuffer(accessToken, channelId, content) {
+  const params = new URLSearchParams();
+  params.append('access_token', accessToken);
+  params.append('profile_ids[]', channelId);
+  params.append('text', content);
+  params.append('now', 'true');
+  const response = await axios.post(`${BUFFER_API}/updates/create.json`, params);
+  if (!response.data.success) throw new Error(response.data.message || 'Echec Buffer');
+  return response.data.updates?.[0];
+}
+
+module.exports = { getBufferChannels, publishNowWithBuffer };
